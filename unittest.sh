b643719e23bce78573aebf7d66933edbbd89d7a8
Pete Lewis <mygit@pjblewis.com>
2013-04-11 00:17:51 -0700

    New unittest.sh

--- unittest.sh
+++ unittest.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-mkdir objs 2> /dev/null
-for f in `ls -1 maps/*.bsp`
-do
-	fn=$(basename "$f")
-	prefix=`echo $fn | awk -F'.' '{ print $1 }'`
-	Debug/bsp2obj.exe "$f" "objs/$prefix.obj"
-	if [ $? -ne 0 ]
-	then
-		exit $?
-	fi
-done
+Debug/bsp2obj.exe "base"
